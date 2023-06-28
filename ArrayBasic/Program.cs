f59c37fba24601ad8830bd7ffc2919454b286742
SnehalS26 <Snehalshirsath26@gmail.com>
2023-06-28 12:24:28 +0530

    Add project files.

---
+++ ArrayBasic/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayBasic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = new string[] { "Snehal", "Nikita", "Swapnil", "Raj", "Pooja" , "Aditi" , "Siddhi" , "Anvi" , "Viraj" };
+            string[] str1 = new string[] { "Pankaj", "Sagar", "Pratik" , "Nikhil" , "Komal" , "Neha"  };
+
+            foreach (var item in str)
+            {
+                Console.WriteLine(item);
+            }
+
+            Array.Sort(str);
+            Console.WriteLine("----After Sort-----");
+            foreach (var item in str)
+            {
+                Console.WriteLine(item);
+            }
+
+            Array.Reverse(str);
+            Console.WriteLine("----After Reverse-----");
+            foreach (var item in str)
+            {
+                Console.WriteLine(item);
+            }
+
+            Array.Copy(str, 2, str1, 2, 3);
+            Console.WriteLine("----Copy-----");
+            foreach (var item in str1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
